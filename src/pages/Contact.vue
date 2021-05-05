73e123d07396e73a0a6c814071c2577e7543aea5
Luca Jungo <luca.jungo@divcom.ch>
2021-05-05 10:59:35 +0200

    Initial commit

---
+++ src/pages/Contact.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-page >
+<h1>Page de contact</h1>
+
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-full.svg"
+    >
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageContact',
+  data () {
+    return {
+      age: 33
+    }
+  }
+}
+</script>
+
+<style lang ="sass" scoped>
+img
+  border: 3pxsolid $secondary
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page >
+<div>
+Je suis <strong>une div</strong> !
+    <p>
+    Je suis <strong>content</strong> !
+    </p>
+</div>
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-full.svg"
+    >
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
+
+<style lang ="sass" scoped>
+img
+  border: 3px solid $secondary
+</style>
