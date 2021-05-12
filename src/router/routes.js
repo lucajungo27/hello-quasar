fd07e646ac62ae0a72ff9afe8d207f4db92b686d
Luca Jungo <luca.jungo@divcom.ch>
2021-05-12 11:44:39 +0200

    Fin exercice 1

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,8 +4,8 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/Index.vue') },
-      { path: 'hello', component: () => import('pages/Hello.vue') },
-      { path: 'ex1', component: () => import('pages/Exercice1.vue') }
+      { path: 'contact', component: () => import('pages/Contact.vue') },
+      { path: 'exercice1', component: () => import('pages/Exercice1.vue') }
     ]
   }
 ]
