7cbbf844f5f9a9ec24db1fbc83f6afb404c9d729
Luca Jungo <luca.jungo@divcom.ch>
2021-05-12 08:26:35 +0200

    Fin exercice 1

--- src/pages/Exercice1.vue
+++ src/pages/Exercice1.vue
@@ -35,43 +35,94 @@
   <q-page padding>
     <div class="form q-mb-lg">
       <div class="row q-mb-md">
-        <label>Nom:</label>
-        <input type="text">
-        <label class="error">Maximum 15 caractères
+        <label>Nom :</label>
+        <label>
+          <input type="text"
+                 v-model="name"
+                 v-autofocus
+                 :class="{'error': !nameIsValid }">
         </label>
+        <label v-show="!nameIsValid"
+               class="error">Maximum 15 caractères</label>
       </div>
       <div class="row q-mb-md">
         <label>Age:</label>
-        <input type="number">
-        <label class="error">Veuillez entrer un âge compris entre 1 et 100</label>
+        <label>
+          <input type="number"
+                 v-model="age"
+                 :class="{'error': !ageIsValid }">
+        </label>
+        <label v-show="!ageIsValid"
+               class="error">Veuillez entrer un âge compris entre 1 et 100</label>
       </div>
       <div class="row">
-        <button>Générer une personne</button>
+        <button @click="randomPerson">Générer une personne</button>
       </div>
     </div>
-    <div class="description q-mb-lg">
-      <p>Mon nom est <b>Danny</b> et j'ai <b>36</b> ans.</p>
-      <p>Dans 10 ans, j'aurai <b>46</b> ans.</p>
-      <p>Mon nom se compose de <b>5</b> caractères.</p>
-      <p>Mon nom en majuscules est <b>DANNY</b>.</p>
+    <div v-if="nameIsValid && ageIsValid"
+         class="description q-mb-lg">
+      <p>Mon nom est <b>{{ name }}</b> et j'ai <b>{{ age }}</b> ans.</p>
+      <p>Dans 10 ans, j'aurai <b>{{ futureAge }}</b> ans.</p>
+      <p>Mon nom se compose de <b>{{ name.length }}</b> caractères.</p>
+      <p>Mon nom en majuscules est <b>{{ name | uppercase }}</b>.</p>
     </div>
-    <div class="no-details">
+    <div v-else class="no-details">
       <p>Veuillez entrer un nom et un âge valide !</p>
     </div>
   </q-page>
 </template>
 
 <script>
+const names = [
+  'Pauline',
+  'Robert',
+  'Jean',
+  'Louise',
+  'Eve',
+  'Katy',
+  'Fritz'
+]
+
 export default {
-  name: 'Exercice 1',
   data () {
     return {
-      name: 'Danny',
-      age: 36
+      name: '',
+      age: ''
     }
+  },
+  computed: {
+    futureAge () {
+      return parseInt(this.age) + 10
+    },
+    nameIsValid () {
+      return this.name.length > 0 && this.name.length <= 15
+    },
+    ageIsValid () {
+      return this.age && this.age > 0 && this.age < 101
+    }
+  },
+  filters: {
+    uppercase: function (value) {
+      return value.toUpperCase()
+    }
+  },
+  methods: {
+    randomPerson () {
+      this.name = names[Math.floor(Math.random() * names.length)]
+      this.age = Math.floor(Math.random() * 100)
+    }
+  },
+  directives: {
+    autofocus: {
+      inserted (el) {
+        el.focus()
+      }
+    }
+  },
+  mounted () {
+    this.randomPerson()
   }
 }
-
 </script>
 
 <style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,21 +1,21 @@
-
 const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/Index.vue') },
-      { path: '/contact', component: () => import('pages/Contact.vue') },
-      { path: '/exercice1', component: () => import('pages/Exercice1.vue') }
+      { path: 'hello', component: () => import('pages/Hello.vue') },
+      { path: 'ex1', component: () => import('pages/Exercice1.vue') }
     ]
-  },
-
-  // Always leave this as last one,
-  // but you can also remove it
-  {
-    path: '*',
-    component: () => import('pages/Error404.vue')
   }
 ]
+
+// Always leave this as last one
+if (process.env.MODE !== 'ssr') {
+  routes.push({
+    path: '*',
+    component: () => import('pages/Error404.vue')
+  })
+}
 
 export default routes
